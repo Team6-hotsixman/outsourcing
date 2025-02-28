efc975bc1ed562e69e46925a29c649af5ba2f284
seungminJ <seungmin103@gmail.com>
2025-02-28 21:53:54 +0900

    feat: Menu, MenuOption 엔티티 추가

---
+++ src/main/java/com/example/outsourcing/domain/menu/entity/Menu.java
@@ -0,0 +1,59 @@
+package com.example.outsourcing.domain.menu.entity;
+
+import com.example.outsourcing.domain.store.entity.Store;
+import com.example.outsourcing.domain.common.entity.Image;
+import com.example.outsourcing.domain.category.entity.Category;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Entity
+@Table(name = "menu")
+@Getter
+@NoArgsConstructor
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String menuName;
+
+    private Integer price;
+
+    private String description;
+
+    private boolean isAvailable;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Builder
+    public Menu(String menuName,
+                Integer price,
+                String description,
+                boolean isAvailable,
+                Store store,
+                Image image,
+                Category category) {
+        this.menuName = menuName;
+        this.price = price;
+        this.description = description;
+        this.isAvailable = isAvailable;
+        this.store = store;
+        this.image = image;
+        this.category = category;
+    }
+}
