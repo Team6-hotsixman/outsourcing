7e944496ba97d53fae112a287c001ab27a8a740b
dlwjdals3667@gmail.com <dlwjdals3667@gmail.com>
2025-02-28 22:25:16 +0900

    feat:User service repository controller 추가

---
+++ src/main/java/com/example/outsourcing/domain/user/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.outsourcing.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+}
---
+++ src/main/java/com/example/outsourcing/domain/user/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.outsourcing.domain.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
