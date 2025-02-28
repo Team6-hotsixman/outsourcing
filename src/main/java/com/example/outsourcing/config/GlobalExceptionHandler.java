6b310c4a45e082d744c043a393959a04cc3b2340
dlwjdals3667@gmail.com <dlwjdals3667@gmail.com>
2025-02-28 21:02:58 +0900

    기초틀

---
+++ src/main/java/com/example/outsourcing/config/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.example.outsourcing.config;
+
+public class GlobalExceptionHandler {
+}
--- src/main/java/com/example/outsourcing/domain/common/entity/BaseEntity.java
+++ src/main/java/com/example/outsourcing/domain/common/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.example.outsourcing.global.entity;
+package com.example.outsourcing.domain.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
