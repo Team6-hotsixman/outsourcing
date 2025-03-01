fb9ec40d3c51b531dcc40e67a55c651e512aa8d2
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-03-01 19:41:02 +0900

    fix : strategy = GenerationType.IDENTITY 수정

--- src/main/java/com/example/outsourcing/domain/common/entity/Image.java
+++ src/main/java/com/example/outsourcing/domain/common/entity/Image.java
@@ -1,9 +1,6 @@
 package com.example.outsourcing.domain.common.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Image extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
