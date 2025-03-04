f5752d04e1ec9469933589a3cb801745bb671e41
seaking110 <seaking110@naver.com>
2025-03-04 20:09:21 +0900

    fix : merge 중 충돌 해결 추가

--- src/main/java/com/example/outsourcing/domain/common/aspect/AdminAspect.java
+++ src/main/java/com/example/outsourcing/domain/common/aspect/AdminAspect.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
